ea0a3596d98b7d3d3bb9a49c949dcc363be86c3a
santosh256reddy <santosh256reddy@gmail.com>
2017-09-01 17:27:49 -0400

    Chat-and-Cluster

---
+++ Node.JS/chat-example-master/chat-and-cluster/README.md
@@ -0,0 +1,7 @@
+# chat-example
+
+This is the source code for a very simple chat example used for 
+the [Getting Started] guide 
+of the Socket.IO website.
+
+Please refer to it to learn how to run this application.
---
+++ Node.JS/chat-example-master/chat-and-cluster/server.js
@@ -0,0 +1,44 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+
+var userCount = 0;
+app.get('/', function(req, res) {
+    res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection',(socket)=>{
+  console.log("User connected");
+  
+  io.emit("updateCount", userCount++);
+  socket.on('disconnect',()=>{
+      io.emit('updateCount',userCount--);
+  });
+    socket.on("sendChat", function(message) {
+
+        console.log('User message:' + message);
+
+        io.emit('receiveChat', message);
+    });
+})
+
+// io.on('connection', function(socket) {
+//     userCount++;
+//     io.emit("updateCount", userCount);
+//     socket.on('disconnect', function() {
+//         console.log('user disconnected');
+//         userCount--;
+//         io.emit("updateCount", userCount);
+//     });
+//     socket.on("LetsChat", function(message) {
+
+//         console.log('User message:' + message);
+
+//         io.emit('LetsChat', message);
+//     });
+// });
+
+http.listen(3000, function() {
+    console.log('listening on *:3000');
+});
